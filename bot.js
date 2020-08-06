28ece45312036d552203d64480e5b7d7c97fac82
emansi <mansielhance@gmail.com>
2020-08-06 22:27:00 +0530

    installed discord.js

---
+++ bot.js
@@ -0,0 +1,14 @@
+const discord = require("discord.js");
+const bot = new discord.Client();
+const token = "NzQwOTUwODU4MzY3MjM4MTc0.XyweQg.y29Rpjmk14tzRioqvZB7TFRjhuQ";
+
+
+bot.on("ready", ()=>{
+    console.log("The bot is online");
+});
+
+
+
+
+
+bot.login(token);
