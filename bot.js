213c4f9438ef56e82060963266bd995660d733e7
emansi <mansielhance@gmail.com>
2020-09-14 15:54:46 +0530

    hugs 0.7

--- bot.js
+++ bot.js
@@ -5,15 +5,15 @@ const prefix = "!";
 
 
 
-bot.on("ready", ()=>{
+bot.on("ready", () => {
     console.log("The bot is online");
-    bot.user.setActivity("Mansi's Port", { type: 'WATCHING'});
+    bot.user.setActivity("Mansi's Port", { type: 'WATCHING' });
     const botNotification = bot.channels.cache.get("740991793650335883");
     botNotification.send(`I'm Online`)
 
 });
 
-bot.on("guildMemberAdd", member=>{
+bot.on("guildMemberAdd", member => {
     const WelcomeChannelId = member.guild.channels.cache.get("740949661451288722");
     WelcomeChannelId.send(`Welcome Aboard Sailor! ${member}`);
 })
@@ -21,53 +21,55 @@ bot.on("guildMemberAdd", member=>{
 
 
 
-bot.on("message", message=>{
+bot.on("message", message => {
     const WelcomeChannelId = message.guild.channels.cache.get("740949661451288722");
     const ServerLogTest = message.guild.channels.cache.get("741006637099384922");
     //Command implementation
     let args = message.content.substring(prefix.length).split(" ")
-    switch(args[0]){
-        case "permit": 
-        var person = message.mentions.members.first();
-        if(!person){
-            message.channel("Mention a valid user");
-            return;
-        }
-        var ShipmateRole = person.guild.roles.cache.get("738872066866020474");
-        if(message.member.hasPermission("MANAGE_NICKNAMES")){
-            person.roles.add(ShipmateRole);
-            message.react("👍");
-            ServerLogTest.send(`${message.author} has permitted ${person}`);
-        }else{
-            message.react("👎");
-        }
-        break;
+    switch (args[0]) {
+        case "permit":
+            var person = message.mentions.members.first();
+            if (!person) {
+                message.channel("Mention a valid user");
+                return;
+            }
+            var ShipmateRole = person.guild.roles.cache.get("738872066866020474");
+            if (message.member.hasPermission("MANAGE_NICKNAMES")) {
+                person.roles.add(ShipmateRole);
+                message.react("👍");
+                ServerLogTest.send(`${message.author} has permitted ${person}`);
+            } else {
+                message.react("👎");
+            }
+            break;
 
     }
 })
 
 
 //automated message
-bot.on("message", message=>{  
+bot.on("message", message => {
+
     // console.log("I'm in the block");
-
     // const LetsTalkChannelId = message.guild.channels.cache.get("738709494493610046");
-   // if (message.channel == LetsTalkChannelId){
-    if (message.content.includes('😂')) {
-        
-        message.channel.send('||scam||');
-       
-        };
+    // if (message.channel == LetsTalkChannelId){
 
-    if(message.content.includes('cry' || 'sad')){
+    if (message.content.includes('😂')) {
+
+        message.channel.send('||scam||');
+
+    };
+
+    //if(message.content.includes('cry' || 'sad')){
+    if (message.content.includes("cry", "CRY", "sad", "SAD")) {
 
         if (message.author.bot) return;
-        else{
+        else {
             message.channel.send('```send HUGS```');
         }
 
     };
-   // };
+    // };
 });
 
 
